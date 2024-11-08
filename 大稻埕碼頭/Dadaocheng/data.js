c631886d2197f58675b43e2186e4a5937a638138
Brian971231147 <Brian971231@gmail.com>
2024-11-08 11:40:53 +0800

    1108-5

---
+++ 大稻埕碼頭/Dadaocheng/data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "環河快速道路景觀",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6998889518724205,
+        "pitch": 0.15730804489491668,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7787558656170575,
+          "pitch": 0.49838724673337254,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.3063227697388022,
+          "pitch": 0.11985098276378814,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5959562109833385,
+          "pitch": -0.191882629539311,
+          "title": "環河快速道路<br>",
+          "text": "環河快速道路，沿淡水河與新店溪修築，於1992年通車，可由此來到大稻埕。"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "大稻埕碼頭貨櫃市集",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1576231987149601,
+          "pitch": 0.485163532225549,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.3114326488579753,
+          "pitch": 0.34513014083531957,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0905584303388487,
+          "pitch": -0.376181017507232,
+          "title": "大稻埕碼頭貨櫃市集",
+          "text": "大稻埕碼頭貨櫃市集擁有絕美的夕陽以及熱鬧人潮。只要是想放鬆或散心，都很適合來到這裡看看美景。不僅有最浪漫的夕陽與河畔風景，還讓你逛得盡興。"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "淡水河",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.811614804448272,
+        "pitch": -0.04775985358479673,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1089195977405524,
+          "pitch": 0.3144311631534169,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.2198924935326225,
+          "pitch": 0.3374000967960775,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8484586989090523,
+          "pitch": -0.5292102296664396,
+          "title": "淡水河",
+          "text": "淡水河於清代可行帆船，可由淡水經萬華達板橋、新莊、三峽至大溪，是先民開墾維生的支柱，至今仍伴隨著大臺北地區所有人的生活。"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "游淡水河船",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 3.137849918355231,
+        "pitch": 0.03389174596739508,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1081617115024285,
+          "pitch": 0.22899207817424205,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        },
+        {
+          "yaw": 3.0408597354750277,
+          "pitch": 0.7055288616753579,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0270971825528328,
+          "pitch": -0.24083676177278335,
+          "title": "游淡水河船",
+          "text": "淡水開港通商後，外商洋行紛紛到此開設商號，促成大稻埕往後的繁華。 現今雖然碼頭功能不復當年，但已轉型為民眾休閒遊憩的去處。"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "自行車道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3553944304905521,
+          "pitch": -0.13178793539377232,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.2146229983523824,
+          "pitch": 0.3630655173138386,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3961688665076686,
+          "pitch": -0.41242669815633626,
+          "title": "自行車道",
+          "text": "從艋舺龍山寺到大稻埕只要30分鐘，沿途河岸影色優美，讓大稻埕非常受歡迎。"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
