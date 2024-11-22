c8a6df7ecf2774b1e3eefd6ce74b2a531f72d2c4
Brian971231147 <Brian971231@gmail.com>
2024-11-22 11:10:16 +0800

    1122

---
+++ Dadaochengvisit/data - 複製.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "台北霞海城隍廟",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5079612988676239,
+        "pitch": -0.04924310565821344,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8033007467048154,
+          "pitch": 0.28615114732993163,
+          "rotation": 7.853981633974483,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.2815569798228061,
+          "pitch": 0.3985929027629673,
+          "rotation": 8.63937979737193,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3951639590490377,
+          "pitch": 0.2518616773058966,
+          "title": "台北霞海城隍廟",
+          "text": "台北霞海城隍廟是大稻埕地區人民重要的信仰中心，被定為三級古蹟，也是台北相當重要的廟宇之一。"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "大稻埕戲院",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4903144555870869,
+        "pitch": -0.16172368121723224,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7198326551683714,
+          "pitch": 0.19470085517560953,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.116151013568617,
+          "pitch": 0.20659030654881505,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2171304447137494,
+          "pitch": 0.028333470432437835,
+          "title": "大稻埕戲苑",
+          "text": "大稻埕戲苑以演出、推廣、保存傳統戲曲為宗旨，辦理各項傳統戲曲演出，亦提供藝文團體租借使用。<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "新芳春茶行",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.517402728528392,
+        "pitch": -0.009786570686237894,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.39791977552186,
+          "pitch": 0.29538490636898906,
+          "rotation": 4.71238898038469,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.8948774976938498,
+          "pitch": 0.3097454246277138,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4006219853860493,
+          "pitch": -0.09674978905401055,
+          "title": "新芳春茶行",
+          "text": "建於1934年，這棟特色洋樓作為大稻埕茶業發展重要的見證。"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "蓬萊國小",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5437156187514525,
+        "pitch": -0.07123930821997426,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6644090509661318,
+          "pitch": 0.4329464577935376,
+          "rotation": 1.5707963267948966,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.8114190664055645,
+          "pitch": 0.289650816169269,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3215604797765224,
+          "pitch": 0.22956418512495347,
+          "title": "蓬萊國小",
+          "text": "建於日治初期，為歷史悠久的學校。有「太平公，蓬萊嬤，日新係阮仔！」，這句北部爺爺奶奶們大多耳熟能詳的諺語，不僅僅是句俏皮話，還一語道盡了大稻埕地區，初等教育的發展特色。"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "寧夏夜市",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 3.0917079289400897,
+        "pitch": 0.160114792495893,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0616807208108012,
+          "pitch": 0.4613593688202453,
+          "rotation": 3.141592653589793,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.7915368505480416,
+          "pitch": 0.17911549035033936,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.917384343816259,
+          "pitch": -0.24915761902895994,
+          "title": "寧夏夜市",
+          "text": "夜市將人車分道，保護行人安全又方便民眾吃飯與購物。而近年來許多攤販能夠使用悠遊卡及行動支付，讓遊客帶著手機就能吃遍夜市美食。"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
